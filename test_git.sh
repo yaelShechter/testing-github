565db9dedd39cbb920aa96c2edc90af58e1eaf9a
Y <Your email>
2021-12-28 14:47:21 +0200

    auto

--- test_git.sh
+++ test_git.sh
@@ -2,7 +2,7 @@
 
 git checkout -b $1
 echo $1 >> hello
-gst
+git status
 git add -A
 git -c user.name="Y" -c user.email="Your email" commit -m "auto"
 git push https://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@github.com/yaelShechter/testing-github $1
